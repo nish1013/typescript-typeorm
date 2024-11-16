13d4fa49506c864e67ab134fa84022d8821a242d
nish1013 <nishand2d@gmail.com>
2024-11-16 14:44:46 +0000

    Restructure UserRepository with instance vars

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -4,17 +4,25 @@ import { UserMapper } from '../domain/user/user.mapper';
 import { UserEntity } from '../entity/user.entity';
 
 export class UserRepository {
-  // Create and save a new user
-  async create(newUser: NewUser): Promise<User> {
-    const user = new UserEntity();
-    user.firstName = newUser.firstName;
-    user.lastName = newUser.lastName;
+  private readonly userRepo = AppDataSource.getRepository(UserEntity);
+  private readonly userMapper = new UserMapper();
 
-    const repo = AppDataSource.getRepository(UserEntity);
-    const created = await repo.save(user);
+  /**
+   * Creates and saves a new user in the database.
+   * @param newUser - The data for the new user.
+   * @returns The created user as a domain object.
+   */
+  public async create(newUser: NewUser): Promise<User> {
+    // Map the input data to an entity
+    const userEntity = this.userRepo.create({
+      firstName: newUser.firstName,
+      lastName: newUser.lastName
+    });
 
-    const mapper = new UserMapper();
+    // Save the entity to the database
+    const savedEntity = await this.userRepo.save(userEntity);
 
-    return mapper.from(created);
+    // Map the saved entity to the domain model
+    return this.userMapper.from(savedEntity);
   }
 }
