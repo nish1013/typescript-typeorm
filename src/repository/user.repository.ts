ae7ad457dd26134a62be2fa419a2f0ffeeb36be1
nish1013 <nishand2d@gmail.com>
2024-11-16 14:34:20 +0000

    Update user.repository.ts to use mapper

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,12 +1,20 @@
 import { AppDataSource } from "../data-source";
+import { NewUser, User } from '../domain/user';
+import { UserMapper } from '../domain/user/user.mapper';
 import { UserEntity } from "../entity/user.entity";
 
 export class UserRepository {
   // Create and save a new user
-  async create(): Promise<UserEntity> {
+  async create(newUser: NewUser): Promise<User> {
     const user = new UserEntity();
+    user.firstName = newUser.firstName;
+    user.lastName = newUser.lastName;
 
     const repo = AppDataSource.getRepository(UserEntity);
-    return repo.save(user);
+    const created = await repo.save(user);
+
+    const mapper = new UserMapper();
+
+    return mapper.from(created);
   }
 }
