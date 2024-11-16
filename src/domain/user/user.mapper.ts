3ad093f9a247507fe06ad9da4fffcff65813de42
nish1013 <nishand2d@gmail.com>
2024-11-16 14:10:46 +0000

    feat: create user.mapper.ts

---
+++ src/domain/user/user.mapper.ts
@@ -0,0 +1,12 @@
+import { UserEntity } from '../../entity/user.entity';
+import { NewUser, User } from './user';
+
+export class UserMapper {
+    public from(entity: UserEntity): User {
+        return {
+            id: entity.id,
+            firstName: entity.firstName,
+            lastName: entity.lastName
+        };
+    }
+}
